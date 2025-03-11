a546e08a07ef5ac7c8cfe3fc8e01290fa0b8eca9
fmnusrat <fmnusrat@yahoo.com>
2025-03-11 00:09:44 +0000

    changed main.tf

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -10,21 +10,6 @@ resource "aws_ecs_cluster" "my-ecs-cluster" {
   name = var.ecs_cluster
 }
 
-data "template_file" "task_definition" {
-  template = file("./task_definition.json")
-
-  vars = {
-    ecr_registry     = var.ecr_registry
-    ecr_repository   = var.ecr_repository
-    image_tag        = var.image_tag
-    execution_role_arn = var.execution_role_arn
-    task_role_arn    = var.task_role_arn
-    container_name   = var.container_name
-    log_group        = var.log_group
-    log_region       = var.aws_region
-  }
-}
-
 resource "aws_ecs_task_definition" "ecs_task" {
   family                   = var.ecs_task_family
   network_mode             = "awsvpc"
