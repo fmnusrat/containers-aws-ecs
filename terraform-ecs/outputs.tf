71307f10a0f8ad5d126d7618d5e39ad12fb4554d
fmnusrat <fmnusrat@yahoo.com>
2025-03-11 17:19:25 +0000

    output fixed

--- terraform-ecs/outputs.tf
+++ terraform-ecs/outputs.tf
@@ -1,11 +1,11 @@
 output "ecs_cluster_id" {
-  value = aws_ecs_cluster.ecs_cluster.id
+  value = aws_ecs_cluster.my_ecs_cluster.id
 }
 
 output "ecs_task_definition_arn" {
-  value = aws_ecs_task_definition.task_role.arn
+  value = aws_ecs_task_definition.ecs_task_def.arn
 }
 
-output "ecs_service" {
-  value = aws_ecs_service.ecs_service.name
+output "ecs_service_name" {
+  value = aws_ecs_service.my_ecs_service.name
 }
