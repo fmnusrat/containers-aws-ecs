c1c16092590ce4ffd09fed4cf764917f8055ea93
fmnusrat <fmnusrat@yahoo.com>
2025-03-18 13:51:03 +0000

    added secrets

--- terraform-ecs/outputs.tf
+++ terraform-ecs/outputs.tf
@@ -2,7 +2,7 @@ output "ecs_cluster_id" {
   value = aws_ecs_cluster.my_ecs_cluster.id
 }
 
-output "ecs_task_definition_arn" {
+output "ecs_task_definition" {
   value = aws_ecs_task_definition.ecs_task_def.arn
 }
 
